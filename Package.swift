8b7cd643b466b801eaca0688567c01adca44d537
ghostflyby <ghostflyby+git@outlook.com>
2025-10-05 00:51:21 +0800

    refactor: rename package from SwiftRime to RimeKit

--- Package.swift
+++ Package.swift
@@ -4,39 +4,39 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftRime",
-    platforms: [
-        .macOS(.v11),
-        .iOS(.v15),
-        .watchOS(.v8),
-        .tvOS(.v15),
-        .macCatalyst(.v15),
-        .visionOS(.v2),
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftRime",
-            targets: ["SwiftRime"])
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .systemLibrary(
-            name: "CLibrime",
-            path: "Sources/CLibrime",
-            pkgConfig: "rime",
-            providers: [
-                .brew(["librime"]),
-                .apt(["librime"]),
-            ],
-        ),
-        .target(
-            name: "SwiftRime",
-            dependencies: ["CLibrime"]),
-        .testTarget(
-            name: "SwiftRimeTests",
-            dependencies: ["SwiftRime"]
-        ),
-    ]
+  name: "RimeKit",
+  platforms: [
+    .macOS(.v11),
+    .iOS(.v15),
+    .watchOS(.v8),
+    .tvOS(.v15),
+    .macCatalyst(.v15),
+    .visionOS(.v2),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "RimeKit",
+      targets: ["RimeKit"])
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .systemLibrary(
+      name: "CLibrime",
+      path: "Sources/CLibrime",
+      pkgConfig: "rime",
+      providers: [
+        .brew(["librime"]),
+        .apt(["librime"]),
+      ],
+    ),
+    .target(
+      name: "RimeKit",
+      dependencies: ["CLibrime"]),
+    .testTarget(
+      name: "RimeKitTests",
+      dependencies: ["RimeKit"]
+    ),
+  ]
 )
