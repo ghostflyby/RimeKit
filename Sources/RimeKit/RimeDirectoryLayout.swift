0956cbaed22135b3664a27fa8287f09fdd1424b3
ghostflyby <ghostflyby+git@outlook.com>
2026-09-11 12:27:18 +0800

    全部源文件补 MPL-2.0 SPDX 标识

--- Sources/RimeKit/RimeDirectoryLayout.swift
+++ Sources/RimeKit/RimeDirectoryLayout.swift
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import Foundation
 
 /// 服务代次:蓝绿双实例的磁盘槽位标识(§4.1)。
--- Tests/RimeKitTests/Support/MinimalRimeData.swift
+++ Tests/RimeKitTests/Support/MinimalRimeData.swift
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: MPL-2.0
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import Foundation
 
 /// 自造最小方案数据(librime gtest 的 data/test/ 范式:不依赖 rime-data/luna_pinyin)。
